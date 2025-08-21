8359eea76a3339386826667d6151e254e5d081ed
mxmlnwbr <maximilian.weber@bluewin.ch>
2025-08-21 15:35:48 +0200

    feat: replace credentials provider with email authentication

--- src/env.js
+++ src/env.js
@@ -15,6 +15,12 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    EMAIL_SERVER: process.env.NODE_ENV === "production"
+      ? z.string()
+      : z.string().optional(),
+    EMAIL_FROM: process.env.NODE_ENV === "production"
+      ? z.string().email()
+      : z.string().email().optional(),
   },
 
   /**
@@ -34,6 +40,8 @@ export const env = createEnv({
     AUTH_SECRET: process.env.AUTH_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    EMAIL_SERVER: process.env.EMAIL_SERVER,
+    EMAIL_FROM: process.env.EMAIL_FROM,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
